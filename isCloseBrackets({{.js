8a4803313255cdc257e95aa68408ce2b6bdcaeba
Danil Pidhainyi <61890854+DanilPidhainyi@users.noreply.github.com>
2023-02-07 16:59:22 +0200

    Update isCloseBrackets({{.js

--- isCloseBrackets({{.js
+++ isCloseBrackets({{.js
@@ -2,7 +2,7 @@ export const isCloseBrackets = str => {
   const brackets = {'(': ')'}
   const closeBrackets = Object.values(brackets)
   let stack = []
-  str.split.('')forEach( el => {
+  str.split('').forEach( el => {
     if (brackets[el]) {
       stack.push(el)
     }
